1fbdef7d0a951769a3880f9a500adde5410b3ace
KingNNT <Dev.KingNNT@gmail.com>
2020-12-02 03:40:03 +0700

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -9,3 +9,28 @@ $(window).scroll(function () {
 		$("header nav").addClass("bg-dark__secondary");
 	}
 });
+
+$(window).scroll(function () {
+	if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+		$("footer").hide();
+	} else {
+		$("footer").show();
+	}
+});
+
+$(document).ready(function () {
+	let btn = $("#button__onTop");
+
+	$(window).scroll(function () {
+		if ($(window).scrollTop() > 300) {
+			btn.show();
+		} else {
+			btn.hide();
+		}
+	});
+
+	btn.on("click", function (e) {
+		e.preventDefault();
+		$("html, body").animate({ scrollTop: 0 }, "300");
+	});
+});
